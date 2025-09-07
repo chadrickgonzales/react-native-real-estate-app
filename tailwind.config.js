2271560a682c3ab21a14ea98f3d56d3ce7259c80
Chadrick Gonzales <chadroelgonzx@gmail.com>
2025-09-07 13:50:58 +0800

    added fonts and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular"],
+        "rubik-bold": ["Rubik-Bold"],
+        "rubik-extraBold": ["Rubik-ExtraBold"],
+        "rubik-semiBold": ["Rubik-SemiBold"],
+        "rubik-medium": ["Rubik-Medium"],
+        "rubik-light": ["Rubik-Light"],
+      },
+
+      colors: {
+        "primary" : {
+          100: '#0061ff0a',
+          200: '#0061ff1a',
+          300: '#0061ff2a',
+        },
+        "accent" : {
+          DEFAULT: '#000000',
+          100: '#8c8e98',
+          200: '#666876',
+          300: '#191d31',
+        },
+        danger: '#f75555'
+      }
+    },
   },
   plugins: [],
 }
